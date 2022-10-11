2e209d6e11e81bc1ef2cc19ed50fdcf674ea8f57
Stephen Olson <48612839+syeve@users.noreply.github.com>
2022-10-11 15:41:49 -0400

    Update procdeletemovie.php

--- procdeletemovie.php
+++ procdeletemovie.php
@@ -8,14 +8,16 @@
 
     require_once("../../../include.php"); // login info to mysql
 
-    $id = $_GET['id'];
-
+    $id = $_GET['id']; // the id of the movie to be deleted
+    
+    // if the deletemovie form was submitted
     if (isset($_POST['deletemovie'])) {
+        // delete the record based on the movie's id
         $stmt = $conn->prepare("DELETE FROM sakila_film WHERE film_id = $id;");
         $stmt->execute();
     }
 
-    header("Location: movietable.php");
+    header("Location: movietable.php"); // return back to the table 
 
     $conn = null; // close connection
 ?>
